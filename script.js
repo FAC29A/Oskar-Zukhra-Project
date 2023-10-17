c4a5c79b353a9d43d7eb7f70c07cbe2bb71d1c4f
Zukhra <zuhra.galieva.2014@gmail.com>
2023-10-17 20:35:02 +0100

    Avoid displaying the header row of the empty table

--- script.js
+++ script.js
@@ -48,19 +48,23 @@ function generateTable(data) {
   const table = document.createElement("table");
   const headers = ["#", "Place", "Magnitude", "Date"];
 
-  // Create table headers
-  const headerRow = document.createElement("tr");
-  headers.forEach((headerText, index) => {
-    const header = document.createElement("th");
-    header.textContent = headerText;
-    // Add ID to the "Magnitude" header
-    if (index === 2) {
-      header.id = "magnitudeHeader";
-    }
+  // Check if there is data to display
+  if (data.length > 0) {
 
-    headerRow.appendChild(header);
-  });
-  table.appendChild(headerRow);
+    // Create table headers
+    const headerRow = document.createElement("tr");
+    headers.forEach((headerText, index) => {
+      const header = document.createElement("th");
+      header.textContent = headerText;
+      // Add ID to the "Magnitude" header
+      if (index === 2) {
+        header.id = "magnitudeHeader";
+      }
+
+      headerRow.appendChild(header);
+    });
+    table.appendChild(headerRow);
+  }
 
   // Create table rows with earthquake information
   data.forEach((earthquake, index) => {
@@ -172,7 +176,9 @@ earthquakeForm.addEventListener("submit", async function (e) {
 
     // Attach event listeners to the specific headers for sorting
     const magnitudeHeader = document.getElementById("magnitudeHeader");
-
+    
+    //Avoid errors when the table is empty and the header row is not displayed
+    if (magnitudeHeader) {
     // Store the original text in a data attribute
     magnitudeHeader.setAttribute("data-original-text", "Magnitude");
 
@@ -186,10 +192,11 @@ earthquakeForm.addEventListener("submit", async function (e) {
       sortTable(2); // Sort by Magnitude
       setSortArrow(magnitudeHeader, sortDirections[2]);
     });
+  }
 
   } catch (error) {
     loader.style.display = "none";
-    countResult.textContent = "An error occurred.";
+    countResult.textContent = "An error occurred";
     console.error("Error:", error);
   }
 });
@@ -221,7 +228,7 @@ function sortTable(column) {
   rows.sort((a, b) => {
     const aValue = a.cells[column].textContent;
     const bValue = b.cells[column].textContent;
-    
+
     // Determine the sorting order based on the column and direction
     const order = sortDirections[column];
 
